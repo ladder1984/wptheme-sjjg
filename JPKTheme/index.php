a653c8ee9325363d649855bbae2f99287da274f3
zeroten <redskydao@gmail.com>
2013-10-30 16:56:35 +0800

    更新友情链接管理

--- JPKTheme/index.php
+++ JPKTheme/index.php
@@ -16,12 +16,9 @@
 
 <div class="link">
           <div class="title"><h2>友情链接</h2></div>
-            <div class="content">
-              <span><a href="http://www.jpk.pku.edu.cn/pkujpk/course/sjjg/">北京大学数据结构与算法网站</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                <span><a href="http://ds.fzu.edu.cn/">福州大学算法与数据结构网站</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                <span><a href="http://jpkc.nwu.edu.cn/datastr/">西北大学数据结构网站</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                <span><a href="http://jsjwhk.tjuci.edu.cn/">大学计算机基础</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                <span><a href="http://yytjjp.tjuci.edu.cn/">应用统计学</a></span>
+            <div class="linkcat">
+               
+               <?php wp_list_bookmarks('title_li='); ?>
           </div>
         </div>
 
